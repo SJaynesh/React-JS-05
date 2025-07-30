8444ae742d69a3df5cfbe8fd1abe2210b23abb05
SJaynesh <jayneshsodvadiya@gmail.com>
2025-07-30 18:48:44 +0530

    Counter App

--- Counter-App/src/App.jsx
+++ Counter-App/src/App.jsx
@@ -1,22 +1,39 @@
+import { useState } from "react";
 import "../public/css/App.css";
+
+
 function App() {
-  let count = 10;
+  // let count = 10;
+  let [count, setCount] = useState(0);
+
+  // Syntax : let [state, function] = useState([defaultValue]);
+
+  console.log("Type : ", typeof (count));
+  console.log("Type : ", typeof (setCount));
+
 
   const increment = () => {
     // alert("Hello World");
-    count = count + 1; // 10 + 1 = 11 + 1 = 12
-    console.log("BTN is clicked...", count);
+    // count = count + 1; // 10 + 1 = 11 + 1 = 12
+    // count = 95;
+    setCount(++count);
+    // console.log("BTN is clicked...", count);
   };
+
+  const decrement = () => {
+    // let newCount = count - 1;
+    count = count - 1;
+    setCount(count);
+  }
 
   return (
     <>
       {/* <h1 style={{ color: "blue", textAlign: "center" }}>Counter App</h1> */}
       <h1>Counter App</h1>
       <div style={{ textAlign: "center" }}>
-        <h3>{count}</h3>
+        <h3>Val : {count}</h3>
         <button onClick={increment}>Increment +</button>
-        <button>Decrement -</button>
-        <button onClick={() => window.print()}>Print</button>
+        <button onClick={decrement}>Decrement -</button>
       </div>
     </>
   );
